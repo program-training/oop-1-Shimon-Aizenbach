4d270ecc3d6e01ea5eba4fbaf6b135fb6a5a1275
Shimon Aizenbach <s0533137261@gmail.com>
2023-08-27 11:20:44 +0300

    stage 2

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 "use strict";
 // את הקוד שלכם תכתבו כאן
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 //  1. Rectangle - מלבן
 var Rectangle = /** @class */ (function () {
@@ -16,6 +31,19 @@ var rectangle1 = new Rectangle(10, 5);
 console.log(rectangle1);
 console.log(rectangle1.area());
 //  2. Square - ריבוע
+var Square = /** @class */ (function (_super) {
+    __extends(Square, _super);
+    function Square(rib) {
+        return _super.call(this, rib, rib) || this;
+    }
+    Square.prototype.area = function () {
+        return this.hight * this.width;
+    };
+    return Square;
+}(Rectangle));
+var square1 = new Square(10);
+console.log(square1);
+console.log(square1.area());
 // 3. Shape - צורה
 // 4. Method Chaining
 // 5. Shape with draw method
--- index.ts
+++ index.ts
@@ -1,27 +1,40 @@
 // את הקוד שלכם תכתבו כאן
 
-import { log } from "console"
+import { log } from "console";
 
 //  1. Rectangle - מלבן
 class Rectangle {
-    hight: number
-    width: number
+  hight: number;
+  width: number;
 
-    constructor(hight: number, width: number) {
-        this.hight = hight
-        this.width = width
-    }
+  constructor(hight: number, width: number) {
+    this.hight = hight;
+    this.width = width;
+  }
 
-    area():number {
-      return this.hight * this.width
-    }
+  area(): number {
+    return this.hight * this.width;
+  }
 }
 
-const rectangle1 = new Rectangle(10, 5)
+const rectangle1 = new Rectangle(10, 5);
 console.log(rectangle1);
 console.log(rectangle1.area());
 
 //  2. Square - ריבוע
+class Square extends Rectangle {
+  constructor(rib: number) {
+    super(rib, rib);
+  }
+
+  area(): number {
+    return this.hight * this.width;
+  }
+}
+
+const square1 = new Square(10);
+console.log(square1);
+console.log(square1.area());
 
 // 3. Shape - צורה
 
