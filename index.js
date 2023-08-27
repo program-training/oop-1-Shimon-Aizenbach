a773352469b4ca8208dfecfeaf8446bc00c83e2e
Shimon Aizenbach <s0533137261@gmail.com>
2023-08-27 11:21:34 +0300

    stage 3

--- index.js
+++ index.js
@@ -17,33 +17,74 @@ var __extends = (this && this.__extends) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 //  1. Rectangle - מלבן
-var Rectangle = /** @class */ (function () {
+// class Rectangle {
+//   hight: number;
+//   width: number;
+//   constructor(hight: number, width: number) {
+//     this.hight = hight;
+//     this.width = width;
+//   }
+//   area(): number {
+//     return this.hight * this.width;
+//   }
+// }
+// const rectangle1 = new Rectangle(10, 5);
+// console.log(rectangle1);
+// console.log(rectangle1.area());
+//  2. Square - ריבוע
+// class Square extends Rectangle {
+//   constructor(rib: number) {
+//     super(rib, rib);
+//   }
+//   area(): number {
+//     return this.hight * this.width;
+//   }
+// }
+// const square1 = new Square(10);
+// console.log(square1);
+// console.log(square1.area());
+// 3. Shape - צורה
+var Shape = /** @class */ (function () {
+    function Shape() {
+    }
+    Shape.prototype.info = function () {
+        return "This is a Shape";
+    };
+    return Shape;
+}());
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
     function Rectangle(hight, width) {
-        this.hight = hight;
-        this.width = width;
+        var _this = this;
+        _this.hight = hight;
+        _this.width = width;
+        return _this;
     }
     Rectangle.prototype.area = function () {
         return this.hight * this.width;
     };
-    return Rectangle;
-}());
-var rectangle1 = new Rectangle(10, 5);
-console.log(rectangle1);
-console.log(rectangle1.area());
-//  2. Square - ריבוע
-var Square = /** @class */ (function (_super) {
-    __extends(Square, _super);
-    function Square(rib) {
-        return _super.call(this, rib, rib) || this;
-    }
-    Square.prototype.area = function () {
-        return this.hight * this.width;
+    Rectangle.prototype.info = function () {
+        return "This is a Rectangle";
     };
-    return Square;
+    return Rectangle;
+}(Shape));
+var ColoredRectangle = /** @class */ (function (_super) {
+    __extends(ColoredRectangle, _super);
+    function ColoredRectangle(hight, width, color) {
+        var _this = _super.call(this, hight, width) || this;
+        _this.color = color;
+        return _this;
+    }
+    ColoredRectangle.prototype.info = function () {
+        return "This is a ".concat(this.color, " Rectangle");
+    };
+    return ColoredRectangle;
 }(Rectangle));
-var square1 = new Square(10);
-console.log(square1);
-console.log(square1.area());
-// 3. Shape - צורה
+var shape1 = new Shape;
+console.log(shape1.info());
+var rectangle1 = new Rectangle(8, 4);
+console.log(rectangle1.info());
+var rectangle2 = new ColoredRectangle(8, 4, "red");
+console.log(rectangle2.info());
 // 4. Method Chaining
 // 5. Shape with draw method
--- index.ts
+++ index.ts
@@ -3,7 +3,47 @@
 import { log } from "console";
 
 //  1. Rectangle - מלבן
-class Rectangle {
+// class Rectangle {
+//   hight: number;
+//   width: number;
+
+//   constructor(hight: number, width: number) {
+//     this.hight = hight;
+//     this.width = width;
+//   }
+
+//   area(): number {
+//     return this.hight * this.width;
+//   }
+// }
+
+// const rectangle1 = new Rectangle(10, 5);
+// console.log(rectangle1);
+// console.log(rectangle1.area());
+
+//  2. Square - ריבוע
+// class Square extends Rectangle {
+//   constructor(rib: number) {
+//     super(rib, rib);
+//   }
+
+//   area(): number {
+//     return this.hight * this.width;
+//   }
+// }
+
+// const square1 = new Square(10);
+// console.log(square1);
+// console.log(square1.area());
+
+// 3. Shape - צורה
+class Shape {
+  info(): string {
+    return `This is a Shape`;
+  }
+}
+
+class Rectangle extends Shape {
   hight: number;
   width: number;
 
@@ -15,28 +55,33 @@ class Rectangle {
   area(): number {
     return this.hight * this.width;
   }
-}
 
-const rectangle1 = new Rectangle(10, 5);
-console.log(rectangle1);
-console.log(rectangle1.area());
-
-//  2. Square - ריבוע
-class Square extends Rectangle {
-  constructor(rib: number) {
-    super(rib, rib);
-  }
-
-  area(): number {
-    return this.hight * this.width;
+  info(): string {
+    return `This is a Rectangle`;
   }
 }
 
-const square1 = new Square(10);
-console.log(square1);
-console.log(square1.area());
+class ColoredRectangle extends Rectangle {
+  color: string;
 
-// 3. Shape - צורה
+  constructor(hight: number, width: number, color: string) {
+    super(hight, width);
+    this.color = color;
+  }
+
+  info(): string {
+      return `This is a ${this.color} Rectangle`
+  }
+}
+
+const shape1 = new Shape
+console.log(shape1.info());
+
+const rectangle1 = new Rectangle(8, 4)
+console.log(rectangle1.info());
+
+const rectangle2 = new ColoredRectangle(8, 4, `red`)
+console.log(rectangle2.info());
 
 // 4. Method Chaining
 
